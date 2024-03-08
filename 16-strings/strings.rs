df05f0e020c9a847f7bf2590673dbffc105b8489
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-03-08 11:38:40 +0100

    Start rework strings

--- 16-strings/strings.rs
+++ 16-strings/strings.rs
@@ -1,23 +1,24 @@
 // Title: Strings
 // Language: rust
 //
-// In Rust there are two main types of strings: string slices (`&str`) and
-// the `String` type. String slices are references to some UTF-8 encoded
-// string data stored elsewhere. String literals are stored in the program's
-// binary and are therefore string slices. The `String` type, on the other hand,
-// is allocated on the heap and is therefore able to store an amount of text
-// that is unknown to us at compile time.
+// In Rust there are two main types of strings: string slices (`&str`) and the
+// `String` type. String slices are references to some UTF-8 encoded string
+// data stored elsewhere. The `String` type, on the other hand, is allocated
+// on the heap and is therefore able to store an amount of text that is unknown
+// to us at compile time.
 
 fn main() {
+    // String slice, an immutable reference to a UTF-8 encoded string in memory
+    // that is not owned by the program. String slices are written as `&str`.
+    let s: &str = "hello, world!";
+    let slice: &str = &s[0..5];
+    println!("{}", slice);
 
     // String literal, fixed size and immutable string slice, and are stored in
-    // the program's binary as immutable data.
+    // the program's binary as immutable data. Therefore, string literals are
+    // considered string slices (`&str`).
     let literal: &str = "hello, world!";
     println!("{}", literal);
-
-    // String slice, an immutable reference to a UTF-8 encoded string in memory
-    let slice: &str = &literal[..5];
-    println!("{}", slice);
 
     // String type, a mutable UTF-8 encoded string allocated on the heap, and
     // can dynamically grow and shrink during runtime.
