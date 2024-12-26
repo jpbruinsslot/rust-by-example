e798b44c90a02603ed2d8c58c6b72f589e5895ed
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-12-26 16:12:10 +0100

    Update ownership

--- 21-ownership/ownership.sh
+++ 21-ownership/ownership.sh
@@ -1,7 +1,9 @@
 # Language: shell
 $ rustc ownership.rs
 $ ./ownership-borrowing
+'hello, world!' is only valid within this scope
 x: 5, y: 5
 z: 42, z_ref: 42
 s5: hello, world!
 took ownership of: hello
+copied: 5
