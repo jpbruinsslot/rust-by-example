d1514cc31a44c89522a59a41458c8291104bfe7a
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-12-26 16:01:43 +0100

    Update ownership

--- 21-ownership/ownership.rs
+++ 21-ownership/ownership.rs
@@ -10,51 +10,67 @@
 #![allow(unused)]
 fn main() {
 
-    // Simple scalar values are **copied** when assigned to another variable.
-    // And can use both variables without issue. Both `x` and `y` are valid and
-    // accessible. If `x` were moved instead of copied, attempting to use `x`
-    // after assigning it to `y` would result in a compile-time error.
+    // Within a scope, a value is valid and accessible. When the scope ends,
+    // the value is dropped and the memory is deallocated.
+    {
+        let msg = String::from("hello, world!");
+        println!("'{}' is only valid within this scope", msg);
+    }
+
+    // Simple scalar values are *copied* when assigned, allowing both variables
+    // to remain valid and accessible. Since scalars are stored on the stack,
+    // the value is duplicated in the new variable. If `x` were moved instead
+    // of copied, using `x` after assigning it to `y` would cause a compile-time
+    // error.
     let x = 5;
     let y = x;
     println!("x: {}, y: {}", x, y);
 
-    // This is different in the following example, here `s1` is **moved** to
-    // `s2`, and **ownership** of `s1` is invalidated, The destination now
-    // controls the value's **lifetime**. When printing `s1` after moving
-    // ownership to `s2` will results in the error:
-    // `error: use of moved value: 's1'`.
+    // In this example, moving `s1` to `s2` invalidates `s1`'s ownership,
+    // transferring control of the value's lifetime to `s2`. Since `String`
+    // stores a pointer on the stack pointing to heap data, only the pointer
+    // is copied during the move, not the heap value. This ensures a single
+    // owner of the value at any time, preventing multiple variables from
+    // pointing to the same content.
     let s1 = String::from("hello");
     let s2 = s1;
 
-    // Using a value without transferring ownership, is called **borrowing**.
-    // Borrowing is done using **references**, which are denoted by `&` and are
-    // immutable by default. You can use the reference to access the value,
-    // and you can reference `z` directly because the ownership was not
-    // transferred, however you cannot modify it.
+    // Using a value without transferring ownership is called **borrowing**, done
+    // via references (`&`), which are immutable by default. A reference allows
+    // access to the value without transferring ownership, but it cannot be
+    // modified.
     let z = 42;
     let z_ref = &z;
     println!("z: {}, z_ref: {}", z, z_ref);
 
-    // When it is necessary to modify a borrowed value, a **mutable reference**
-    // is used, denoted by `&mut`. Mutable references allow modification of these
-    // borrowed variable's value without transferring ownership.
+    // To modify a borrowed value, use a mutable reference (`&mut`), which
+    // allows changes without transferring ownership.
     let mut s4 = String::from("hello");
     let s5 = &mut s4;
     *s5 = String::from("hello, world!");
     println!("s5: {}", s5);
 
-    // Passing a value to function *moves* or *copies* it, depending on the type.
-    // In this example, the value is **moved** into the function and ownership
-    // is transferred, invalidating the original variable, which causes an error
-    // if used afterwards: `error: value borrowed here after move`.
+    // Passing a value to a function also *moves* or *copies* it based on its
+    // type. For the `String` type , the value is moved, transferring ownership
+    // to the function and invalidating the original variable, as `String` data
+    // is stored on the heap.
     fn takes_ownership(s: String) {
         println!("took ownership of: {}", s);
     }
     let s6 = String::from("hello");
     takes_ownership(s6);
 
-    // Also returning a value from a function moves or copies it, depending on
-    // the type. Here the return value is moved into `s7`.
+    // Scalar values are copied when passed to a function, and the original
+    // variable remains valid. This is because scalar values are stored on the
+    // stack, and the value is copied into the function.
+    fn copies(x: i32) {
+        println!("copied: {}", x);
+    }
+    let x = 5;
+    copies(x);
+
+    // Returning a value from a function also *moves* or *copies* it, depending
+    // on the type. Here the return value is moved into `s7`.
     fn gives_ownership() -> String {
         let s = String::from("hello");
         return s;
@@ -63,8 +79,7 @@ fn main() {
 
     // A combination of both moving and returning a value is also possible.
     // Here, `s8` is moved into the function, which then moves its
-    // return value into `s9`. Using `s8` afterward results in the error:
-    // `error: value borrowed here after move`.
+    // return value into `s9`.
     fn takes_and_gives_back(s: String) -> String {
         return s;
     }
@@ -72,17 +87,16 @@ fn main() {
     let s9 = takes_and_gives_back(s8);
 
     // When we want to allow a function to use a value without taking ownership
-    // we can achieve that by using references. The function takes a *reference*
-    // `&String`, leaving the original variable valid, a process called
-    // **borrowing**.
+    // we can achieve that by also using references. The function takes a
+    // *reference* `&String`, leaving the original variable valid, a process
+    // called **borrowing**.
     fn calculate_length(s: &String) -> usize {
         return s.len();
     }
     let s10 = String::from("hello");
     let length = calculate_length(&s10);
 
-    // References (`&)` are immutable by default and cannot be modified,
-    // leading to the error: `error: cannot borrow as mutable`. Mutable
+    // Mutable
     // references (`&mut`) allow value modification without transferring
     // ownership.
     fn change(s: &mut String) {
